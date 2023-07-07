012c4f72ec27375cc77d82b6eed783df4a8cff9e
Gustavo Penagos <110733999+GustavoPenagos@users.noreply.github.com>
2023-07-07 15:24:47 -0500

    CreacionXMLFormat: punto 2

---
+++ Colegio/Controllers/XMLFormatController.cs
@@ -0,0 +1,41 @@
+﻿using Colegio.Models;
+using Colegio.Models.XMLFormat;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Colegio.Controllers
+{
+    public class XMLFormatController : Controller
+    {
+       public async Task<dynamic> XMLFormat()
+        {
+            try
+            {
+                List<XMLFormat> xmlList = new List<XMLFormat>();
+                HttpClient client = new HttpClient();
+                string apiCrud = System.Configuration.ConfigurationManager.AppSettings["UrlAPI"] + "api/XML";
+                HttpResponseMessage httpResponse = await client.GetAsync(apiCrud);
+                if(httpResponse.IsSuccessStatusCode)
+                {
+                    string response = await httpResponse.Content.ReadAsStringAsync();
+                    List<XMLFormat> format = JsonConvert.DeserializeObject<List<XMLFormat>>(response);
+                    
+
+                    return View(format);
+                }
+                return View();
+            }
+            catch(Exception ex)
+            {
+                return View("Error", ex.Message);
+            }
+        }
+    }
+}
---
+++ Colegio/Models/XMLFormat/XMLFormat.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Colegio.Models.XMLFormat
+{
+    public class XMLFormat
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public decimal Valor { get; set; }
+        public string Iva { get; set; }
+    }
+}
